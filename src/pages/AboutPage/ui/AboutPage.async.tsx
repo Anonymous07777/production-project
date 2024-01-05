1bcf3193043bb4e1d18978bbc63c2b857f2b7191
Anonymous07777 <abaraev2003@gmail.com>
2024-01-05 15:34:29 +0500

    Fix file

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,6 +2,6 @@ import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
-    // Это исскуственная задержка,так делать не надо. Надо просто () => import('./AboutPage') в lazy
+    // Это исскуственная задержка,та делать не надо. Надо просто () => import('./AboutPage') в lazy
     setTimeout(() => resolve(import('./AboutPage')), 2000);
 }));
