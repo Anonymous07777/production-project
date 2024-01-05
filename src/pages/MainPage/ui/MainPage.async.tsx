7b7656c3b371e7d90ed2b903421c75ddd8141077
Anonymous07777 <abaraev2003@gmail.com>
2024-01-05 14:50:35 +0500

    fix files

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,7 @@
-import {lazy} from "react";
+import { lazy } from 'react';
 
-export const MainPageAsync = lazy(() => new Promise(resolve => {
-    //@ts-ignore
+export const MainPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
     // Это исскуственная задержка, так делать не надо. Надо просто () => import('./MainPage') в lazy
-    setTimeout(() => resolve(import('./MainPage')), 2000)
+    setTimeout(() => resolve(import('./MainPage')), 2000);
 }));
